ad23d0232e39b69a829247ce72ed060abc4432cf
xbxbxb123 <benjaminrgoodwin@gmail.com>
2024-05-24 20:16:09 -0700

    Attempted to add cut titanium
    This is my first time doing true programming for a minecraft mod. Lets hope I didn't break anything!

--- src/main/java/dev/dhyces/lunarnether/registry/ModBlocks.java
+++ src/main/java/dev/dhyces/lunarnether/registry/ModBlocks.java
@@ -11,28 +11,38 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModBlocks {
     public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(Registries.BLOCK, LunarNether.MODID);
 
-    public static final RegistryObject<Block> LUNAR_DUST = REGISTRY.register("lunar_dust", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE).strength(0.5f).sound(SoundType.SAND)));
+    //Moondust
+    public static final RegistryObject<Block> LUNAR_DUST = REGISTRY.register("lunar_dust", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.CLAY).strength(0.5f).sound(SoundType.SAND)));
     
-    public static final RegistryObject<Block> LUNAR_STONE = REGISTRY.register("lunar_stone", () -> new Block(BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
-    public static final RegistryObject<Block> LUNAR_STONE_STAIRS = REGISTRY.register("lunar_stone_stairs", () -> new StairBlock(() -> LUNAR_STONE.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
-    public static final RegistryObject<Block> LUNAR_STONE_SLAB = REGISTRY.register("lunar_stone_slab", () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
-    public static final RegistryObject<Block> LUNAR_STONE_WALL = REGISTRY.register("lunar_stone_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
+    //Moonstone
+    public static final RegistryObject<Block> LUNAR_STONE = REGISTRY.register("lunar_stone", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
+    public static final RegistryObject<Block> LUNAR_STONE_STAIRS = REGISTRY.register("lunar_stone_stairs", () -> new StairBlock(() -> LUNAR_STONE.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
+    public static final RegistryObject<Block> LUNAR_STONE_SLAB = REGISTRY.register("lunar_stone_slab", () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
+    public static final RegistryObject<Block> LUNAR_STONE_WALL = REGISTRY.register("lunar_stone_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
 
-    public static final RegistryObject<Block> POLISHED_LUNAR_STONE = REGISTRY.register("polished_lunar_stone", () -> new Block(BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
-    public static final RegistryObject<Block> POLISHED_LUNAR_STONE_STAIRS = REGISTRY.register("polished_lunar_stone_stairs", () -> new StairBlock(() -> POLISHED_LUNAR_STONE.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
-    public static final RegistryObject<Block> POLISHED_LUNAR_STONE_SLAB = REGISTRY.register("polished_lunar_stone_slab", () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
-    public static final RegistryObject<Block> POLISHED_LUNAR_STONE_WALL = REGISTRY.register("polished_lunar_stone_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
+    //Polished Moonstone
+    public static final RegistryObject<Block> POLISHED_LUNAR_STONE = REGISTRY.register("polished_lunar_stone", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
+    public static final RegistryObject<Block> POLISHED_LUNAR_STONE_STAIRS = REGISTRY.register("polished_lunar_stone_stairs", () -> new StairBlock(() -> POLISHED_LUNAR_STONE.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
+    public static final RegistryObject<Block> POLISHED_LUNAR_STONE_SLAB = REGISTRY.register("polished_lunar_stone_slab", () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
+    public static final RegistryObject<Block> POLISHED_LUNAR_STONE_WALL = REGISTRY.register("polished_lunar_stone_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
+    
+    //Cut Polished Moonstone
+    public static final RegistryObject<Block> CUT_POLISHED_LUNAR_STONE = REGISTRY.register("cut_polished_lunar_stone", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
+    public static final RegistryObject<Block> CUT_POLISHED_LUNAR_STONE_STAIRS = REGISTRY.register("cut_polished_lunar_stone_stairs", () -> new StairBlock(() -> CUT_POLISHED_LUNAR_STONE.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
+    public static final RegistryObject<Block> CUT_POLISHED_LUNAR_STONE_SLAB = REGISTRY.register("cut_polished_lunar_stone_slab", () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
+    public static final RegistryObject<Block> CUT_POLISHED_LUNAR_STONE_WALL = REGISTRY.register("cut_polished_lunar_stone_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE).mapColor(MapColor.QUARTZ)));
 
-    public static final RegistryObject<Block> CUT_POLISHED_LUNAR_STONE = REGISTRY.register("cut_polished_lunar_stone", () -> new Block(BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
-    public static final RegistryObject<Block> CUT_POLISHED_LUNAR_STONE_STAIRS = REGISTRY.register("cut_polished_lunar_stone_stairs", () -> new StairBlock(() -> CUT_POLISHED_LUNAR_STONE.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
+    //Ilmenite
+    public static final RegistryObject<Block> ILMENITE_ORE = REGISTRY.register("ilmenite_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_DIAMOND_ORE).mapColor(MapColor.STONE)));
+    public static final RegistryObject<Block> RAW_ILMENITE_BLOCK = REGISTRY.register("raw_ilmenite_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.RAW_IRON_BLOCK).strength(5.0F, 16.0F)));
 
+    //Titanium
+    public static final RegistryObject<Block> TITANIUM_BLOCK = REGISTRY.register("titanium_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.NETHERITE_BLOCK).strength(10.0F, 30.0F).mapColor(MapColor.METAL)));
 
-    public static final RegistryObject<Block> CUT_POLISHED_LUNAR_STONE_SLAB = REGISTRY.register("cut_polished_lunar_stone_slab", () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
-    public static final RegistryObject<Block> CUT_POLISHED_LUNAR_STONE_WALL = REGISTRY.register("cut_polished_lunar_stone_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
-
-    public static final RegistryObject<Block> ILMENITE_ORE = REGISTRY.register("ilmenite_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.END_STONE).mapColor(MapColor.STONE)));
-    public static final RegistryObject<Block> RAW_ILMENITE_BLOCK = REGISTRY.register("raw_ilmenite_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.RAW_IRON_BLOCK).mapColor(MapColor.RAW_IRON)));
-    public static final RegistryObject<Block> TITANIUM_BLOCK = REGISTRY.register("titanium_block", () -> new Block(BlockBehaviour.Properties.copy(Blocks.NETHERITE_BLOCK)));
-
+    //Cut Titanium
+    public static final RegistryObject<Block> CUT_TITANIUM = REGISTRY.register("cut_titanium", () -> new Block(BlockBehaviour.Properties.copy(Blocks.NETHERITE_BLOCK).strength(8.0F, 25.0F).mapColor(MapColor.METAL)));
+    public static final RegistryObject<Block> CUT_TITANIUM_STAIRS = REGISTRY.register("cut_titanium_stairs", () -> new StairBlock(() -> CUT_TITANIUM.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.NETHERITE_BLOCK).strength(8.0F, 25.0F).mapColor(MapColor.METAL)));
+    public static final RegistryObject<Block> CUT_TITANIUM_SLAB = REGISTRY.register("cut_titanium_slab", () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.NETHERITE_BLOCK).strength(8.0F, 25.0F).mapColor(MapColor.METAL)));
+    public static final RegistryObject<Block> CUT_TITANIUM_WALL = REGISTRY.register("cut_titanium_wall", () -> new WallBlock(BlockBehaviour.Properties.copy(Blocks.NETHERITE_BLOCK).strength(8.0F, 25.0F).mapColor(MapColor.METAL)));
 
 }
--- src/main/java/dev/dhyces/lunarnether/registry/ModItems.java
+++ src/main/java/dev/dhyces/lunarnether/registry/ModItems.java
@@ -29,9 +29,16 @@ public class ModItems {
     public static final RegistryObject<Item> CUT_POLISHED_LUNAR_STONE_STAIRS = registerBlockItem("cut_polished_lunar_stone_stairs", ModBlocks.CUT_POLISHED_LUNAR_STONE_STAIRS);
     public static final RegistryObject<Item> CUT_POLISHED_LUNAR_STONE_SLAB = registerBlockItem("cut_polished_lunar_stone_slab", ModBlocks.CUT_POLISHED_LUNAR_STONE_SLAB);
     public static final RegistryObject<Item> CUT_POLISHED_LUNAR_STONE_WALL = registerBlockItem("cut_polished_lunar_stone_wall", ModBlocks.CUT_POLISHED_LUNAR_STONE_WALL);
+
     public static final RegistryObject<Item> ILMENITE_ORE = registerBlockItem("ilmenite_ore", ModBlocks.ILMENITE_ORE);
     public static final RegistryObject<Item> RAW_ILMENITE_BLOCK = registerBlockItem("raw_ilmenite_block", ModBlocks.RAW_ILMENITE_BLOCK);
+
     public static final RegistryObject<Item> TITANIUM_BLOCK = registerBlockItem("titanium_block", ModBlocks.TITANIUM_BLOCK);
+
+    public static final RegistryObject<Item> CUT_TITANIUM = registerBlockItem("cut_titanium", ModBlocks.CUT_TITANIUM);
+    public static final RegistryObject<Item> CUT_TITANIUM_STAIRS = registerBlockItem("cut_titanium_stairs", ModBlocks.CUT_TITANIUM_STAIRS);
+    public static final RegistryObject<Item> CUT_TITANIUM_SLAB = registerBlockItem("cut_titanium_slab", ModBlocks.CUT_TITANIUM_SLAB);
+    public static final RegistryObject<Item> CUT_TITANIUM_WALL = registerBlockItem("cut_titanium_wall", ModBlocks.CUT_TITANIUM_WALL);
 
     public static final RegistryObject<Item> RAW_ILMENITE = REGISTRY.register("raw_ilmenite", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> TITANIUM_INGOT = REGISTRY.register("titanium_ingot", () -> new Item(new Item.Properties()));
