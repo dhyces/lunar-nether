3a06e6acd77ddd3b10ee2a037bffb943a8e92a77
xbxbxb123 <benjaminrgoodwin@gmail.com>
2024-08-11 23:22:28 -0700

    Update ModCreativeModTabs.java

--- src/main/java/dev/dhyces/lunarnether/registry/ModCreativeModTabs.java
+++ src/main/java/dev/dhyces/lunarnether/registry/ModCreativeModTabs.java
@@ -1,5 +1,14 @@
 package dev.dhyces.lunarnether.registry;
 
+import dev.dhyces.lunarnether.LunarNether;
+import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
 import dev.dhyces.lunarnether.registry.ModItems;
 
 public class ModCreativeModTabs { 
