b0806902b1ff5899c4741172b9a4511ce12c65a1
xbxbxb123 <benjaminrgoodwin@gmail.com>
2024-08-11 22:47:16 -0700

    Update ModCreativeModTabs.java

--- src/main/java/dev/dhyces/lunarnether/registry/ModCreativeModTabs.java
+++ src/main/java/dev/dhyces/lunarnether/registry/ModCreativeModTabs.java
@@ -6,7 +6,7 @@ public class ModCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = 
         DeferredRegister.create(Registries.CREATIVE_MODE_TAB, LunarNether.MODID);
 
-        public static final RegistryObject<CreativeModeTab> MOON_TAB = CREATIVE_MODE_TABS.register("moon_tab", () -> CreativeModeTab.builder().icon(()-> new ItemStack(ModItems.Moonstone.get()))
+        public static final RegistryObject<CreativeModeTab> MOON_TAB = CREATIVE_MODE_TABS.register("moon_tab", () -> CreativeModeTab.builder().icon(()-> new ItemStack(ModItems.LUNAR_STONE.get()))
         .title(Component.translatable("creativetab.moon_tab"))
         .displayItems((pParameters,pOutput) -> {
             pOutput.accept(ModItems.LUNAR_STONE.get());
