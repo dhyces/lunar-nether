01313e01b4f2589381ab063e22d755927e3e22fb
xbxbxb123 <benjaminrgoodwin@gmail.com>
2024-08-11 22:36:26 -0700

    Update ModCreativeModTabs.java

--- src/main/java/dev/dhyces/lunarnether/registry/ModCreativeModTabs.java
+++ src/main/java/dev/dhyces/lunarnether/registry/ModCreativeModTabs.java
@@ -9,7 +9,7 @@ public class ModCreativeModTabs {
         public static final RegistryObject<CreativeModeTab> MOON_TAB = CREATIVE_MODE_TABS.register("moon_tab", () -> CreativeModeTab.builder().icon(()-> new ItemStack(ModItems.Moonstone.get()))
         .title(Component.translatable("creativetab.moon_tab"))
         .displayItems((itemDisplayParameters,output) -> {
-            output.accept(ModItems.ITEMNAME.get());
+            output.accept(ModItems.LUNAR_STONE.get());
         })
         .getItems()
         .build());
