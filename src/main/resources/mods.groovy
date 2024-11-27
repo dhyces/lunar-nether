b7ef6e1639ddc7eef9457f0f5541cf6d2c3d91df
dhyces <10985914+dhyces@users.noreply.github.com>
2024-11-27 18:46:17 -0500

    Update build tools to 1.21

---
+++ src/main/resources/mods.groovy
@@ -0,0 +1,31 @@
+MultiplatformModsDotGroovy.make {
+    def modid = buildProperties["mod_id"]
+
+    modLoader = "javafml"
+    loaderVersion = libs.versions.get("javafml_range")
+
+    license = buildProperties["mod_license"]
+    issueTrackerUrl = buildProperties["mod_issues_url"]
+
+    mod {
+        modId = modid
+        displayName = buildProperties["mod_name"]
+        authors = [(buildProperties["authors"] as String)]
+        version = environmentInfo.version
+
+        displayUrl = buildProperties["mod_display_url"]
+        sourcesUrl = buildProperties["mod_source_url"]
+        logoFile = "assets/${modid}/logo.png"
+        description = buildProperties["mod_description"]
+
+        dependencies {
+            mod("neoforge") {
+                versionRange = "${libs.versions.get("neoforge_range")}"
+            }
+        }
+    }
+
+    mixins {
+        mixin("${modid}.mixins.json")
+    }
+}
