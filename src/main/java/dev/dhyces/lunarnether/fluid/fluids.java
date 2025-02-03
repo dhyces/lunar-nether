573b73333c8b5833cccf56f2f1e7f62923bd9c8c
xbxbxb123 <benjaminrgoodwin@gmail.com>
2025-02-02 22:06:11 -0800

    Attempted to add tinkers compatibility
    the fluid is completely broken, there are a bunch of things that need to be done for it that I don't get

---
+++ src/main/java/dev/dhyces/lunarnether/fluid/fluids.java
@@ -0,0 +1,5 @@
+//import (a bunch of things, idk which are important)
+
+public static final FlowingFluidObject<ForgeFlowingFluid> moltenTitanium = FLUIDS.register("molten_titanium").type(hot("molten_titanium").temperature(1300).lightLevel(10)).block(createBurning(MapColor.IRON, 8, 10, 6f)).bucket().commonTag().flowing();
+
+//THIS IS COMPLETELY BROKEN
